eeb3fc65df743bf3ff1d60f55da8c3ac6931e09e
Ogundele Caleb <ogundelecaleb13@gmail.com>
2022-10-05 00:15:47 +0100

    first commit

---
+++ components/Cart.jsx
@@ -0,0 +1,203 @@
+import React, { useRef, useState } from "react";
+import Link from "next/link";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineLeft,
+  AiOutlineShopping,
+} from "react-icons/ai";
+import { TiDeleteOutline } from "react-icons/ti";
+import { useStateContext } from "../context/StateContext";
+import { urlFor } from "../lib/client";
+// import { PaystackButton } from "next-paystack";
+
+// import { usePaystackPayment } from "next-paystack";
+
+// const config = {
+//     reference: (new Date()).getTime().toString(),
+//     email: "ogundelecaleb13@gmail.com.com",
+//     amount: 2000,
+//     publicKey: 'pk_live_92702818cd044d1b12c0f4c464a5502f123ebc35',//Note:
+
+// };
+
+// // you can call this function anything
+// const onSuccess = (reference) => {
+//   // Implementation for whatever you want to do with reference and after success call.
+//   console.log(reference);
+// };
+
+// // you can call this function anything
+// const onClose = () => {
+//   // implementation for  whatever you want to do when the Paystack dialog closed.
+//   console.log('closed')
+// }
+
+// const PaystackHookExample = () => {
+//     const initializePayment = usePaystackPayment(config);
+//     return (
+//       <div>
+//           <button onClick={() => {
+//               initializePayment(onSuccess, onClose)
+//           }}>Paystack Hooks Implementation</button>
+//       </div>
+//     );
+// };
+
+// const handleCheckout = () => {
+//   <PaystackHookExample />
+// }
+
+const Cart = () => {
+  const cartRef = useRef();
+  const {
+    totalPrice,
+    totalQuantities,
+    cartItems,
+    setShowCart,
+    toggleCartItemQuanitity,
+    onRemove,
+  } = useStateContext();
+
+  // const [paystackHook, setPaystackHook] = useState({
+  //   key: "pk_live_92702818cd044d1b12c0f4c464a5502f123ebc35",
+  //   email: "foobar@example.com",
+  //   amount: totalPrice ,
+  // });
+  // const callback = (response) => {
+  //   console.log(response); // card charged successfully, get reference here
+  // };
+
+  // const close = () => {
+  //   console.log("Payment closed");
+  // };
+
+  // const getReference = () => {
+  //   //you can put any unique reference implementation code here
+  //   let text = "";
+  //   let possible =
+  //     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.=";
+
+  //   for (let i = 0; i < 15; i++)
+  //     text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  //   return text;
+  // };
+
+  return (
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Your shopping bag is empty</h3>
+            <Link href="/">
+              <button
+                type="button"
+                onClick={() => setShowCart(false)}
+                className="btn"
+              >
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
+
+        <div className="product-container">
+          {cartItems.length >= 1 &&
+            cartItems.map((item) => (
+              <div className="product" key={item._id}>
+                <img
+                  src={urlFor(item?.image[0])}
+                  className="cart-product-image"
+                />
+                <div className="item-desc">
+                  <div className="flex top">
+                    <h5>{item.name}</h5>
+                    <h4>${item.price}</h4>
+                  </div>
+                  <div className="flex bottom">
+                    <div>
+                      <p className="quantity-desc">
+                        <span
+                          className="minus"
+                          onClick={() =>
+                            toggleCartItemQuanitity(item._id, "dec")
+                          }
+                        >
+                          <AiOutlineMinus />
+                        </span>
+                        <span className="num" onClick="">
+                          {item.quantity}
+                        </span>
+                        <span
+                          className="plus"
+                          onClick={() =>
+                            toggleCartItemQuanitity(item._id, "inc")
+                          }
+                        >
+                          <AiOutlinePlus />
+                        </span>
+                      </p>
+                    </div>
+                    <button
+                      type="button"
+                      className="remove-item"
+                      onClick={() => onRemove(item)}
+                    >
+                      <TiDeleteOutline />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+        {cartItems.length >= 1 && (
+          <div className="cart-bottom">
+            <div className="total">
+              <h3>Subtotal:</h3>
+              <h3>#{totalPrice}</h3>
+            </div>
+            <div className="btn-container">
+              <Link href='/paymentform'>
+              <button onClick={() => setShowCart(false)}>
+                pay now
+              </button>
+
+              </Link>
+
+
+              {/* <PaystackButton
+                text="Checkout"
+                
+                callback={callback}
+                close={close}
+                disabled={true}
+                embed={true}
+                reference={getReference()}
+                email={paystackHook.email}
+                amount={paystackHook.amount}
+                publicKey={"pk_live_92702818cd044d1b12c0f4c464a5502f123ebc35"}
+                tag="button"
+              /> */}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
+
+// {<Paystack totalPrice={totalPrice}/>}
---
+++ pages/paymentform.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import { useStateContext } from "../context/StateContext";
+import { PaystackButton } from "next-paystack";
+import { urlFor } from "../lib/client";
+
+const Paymentform = () => {
+  const { totalPrice, cartItems } = useStateContext();
+
+  const publicKey = "pk_test_f74979a16e937b35fa78b60d796d8d19171d70b4";
+  const amount = totalPrice * 100;
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  
+  const custom_fields = [
+    {
+      display_name: { phone },
+      variable_name: { name },
+      value: {email}
+    },
+    {
+      display_name: { name },
+      variable_name: { name },
+      
+    }
+  ];
+  const componentProps = {
+    email,
+    amount,
+    metadata: {
+      custom_fields,
+    },
+    publicKey,
+    callback_url: "/success",
+    text: "Buy Now",
+    onSuccess: () => {
+      setEmail("");
+      setName("");
+      setPhone("");
+    },
+    onClose: () => alert("Wait! You need this oil, don't go!!!!"),
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="item">
+          <div className="overlay-effect"></div>
+          {cartItems.map((item) => (
+            <div className="product" key={item._id}>
+              <img
+                src={urlFor(item?.image[0])}
+                className="cart-product-image"
+              />
+              <div className="item-desc">
+                <div className="flex top">
+                  <h5>{item.name}</h5>
+                  <h4>#{item.price}</h4>
+                </div>
+              </div>
+            </div>
+          ))}
+
+          <div className="item-details">
+            <p className="item-details__title">Total:</p>
+            <p className="item-details__amount">NGN {totalPrice}</p>
+          </div>
+        </div>
+        <div className="checkout">
+          <div className="checkout-form">
+            <div className="checkout-field">
+              <label>Name</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="checkout-field">
+              <label>Email</label>
+              <input
+                type="text"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="checkout-field">
+              <label>Phone</label>
+              <input
+                type="text"
+                id="phone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+            <PaystackButton className="paystack-button" {...componentProps} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Paymentform;
