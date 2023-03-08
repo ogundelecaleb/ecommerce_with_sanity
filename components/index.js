34f47fd73382f7d72ebefe68fc1b158d0d8f88dc
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-03-09 00:01:06 +0100

    NEW

--- components/index.js
+++ components/index.js
@@ -3,6 +3,6 @@ export { default as Layout } from './Layout';
 export { default as Navbar } from './Navbar';
 export { default as Product } from './Product';
 
-export { default as HeroBanner } from './HeroBanner';
+export { default as HeroBanner } from './Hero';
 export { default as FooterBanner } from './FooterBanner';
 export { default as Cart } from './Cart';
